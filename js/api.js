4d8018b020da80fcaa0b84709008a95e2bd93c42
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-07-23 22:00:46 +0600

    load data from api and send api data to a function

--- js/api.js
+++ js/api.js
@@ -3,3 +3,21 @@ function loadData() {
     .then((response) => response.json())
     .then((json) => console.log(json, json.title));
 }
+
+function loadUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    //sending data to a function
+    .then((data) => displayPosts(data));
+}
+
+//below function will get data parameter from json data
+function displayPosts(datam) {
+  console.log(datam);
+}
