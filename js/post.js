423482becd27592274175296f77a8a4336a3d5a3
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-07-24 08:42:38 +0600

    loaded post from api and applying css style

--- js/post.js
+++ js/post.js
@@ -1,1 +1,21 @@
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayData(data));
+}
+// must call function to see the data;
+loadPosts();
 
+function displayData(posts) {
+  const section = document.getElementById("posts");
+  for (const post of posts) {
+    const div = document.createElement("div");
+    div.classList.add("post");
+    div.innerHTML = `
+      <h2>${post.title}</h2>
+      <p>${post.body}</p>
+      `;
+    section.appendChild(div);
+    console.log(post);
+  }
+}
