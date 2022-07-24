ea1308432d9ee7ad1d45f1066b6dd2da9d49862b
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-07-24 12:55:40 +0600

    quiz mod32 added

---
+++ quiz-mod-32.js
@@ -0,0 +1,7 @@
+//q1
+const stuInfo = JSON.stringify({ name: "James", roll: 3 });
+console.log(stuInfo.name);
+//q2
+const premikas = { name: "keka ferdousi", cars: { brand: "toyota" } };
+const { brand } = premikas.cars;
+console.log(brand);
